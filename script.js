cca7fec25bffeee2bd2fac789971a2004cccfab0
tangguhriyadi <mtangguh97@gmail.com>
2022-08-06 11:47:55 +0700

    clear

---
+++ script.js
@@ -0,0 +1,42 @@
+const state = {
+    displayNumber: 'Result',
+    operator: null,
+    firstNumber: null,
+};
+
+const updateDisplay = () => {
+    document.querySelector('#display').innerText = state.displayNumber
+}
+
+const buttons = document.querySelectorAll('.box')
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case '=':
+                try{
+                let result = eval(state.displayNumber)
+                state.displayNumber = result
+                }catch(error){
+                    state.displayNumber = 'NaN, coba lagi'
+                    setTimeout(() => {
+                        state.displayNumber = 'Result'
+                        updateDisplay()
+                    }, 2000)
+                }
+                
+            updateDisplay()
+            break;
+            case 'C':
+            state.displayNumber = 'Result'
+            updateDisplay()
+            break
+            default:
+                if(state.displayNumber === 'Result'){
+                    state.displayNumber = e.target.innerText
+                } else {
+                    state.displayNumber += e.target.innerText
+                }
+                updateDisplay()
+        }
+    })
+}
